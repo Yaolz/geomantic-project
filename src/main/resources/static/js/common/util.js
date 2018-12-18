58c838b106fac1abe313af543b6310bed0e87ad9
ZhengYouzhen <1006747577@qq.com>
2018-12-18 14:42:17 +0800

    util.js

--- src/main/resources/static/js/common/util.js
+++ src/main/resources/static/js/common/util.js
@@ -8,45 +8,37 @@
 //    var _obj = NaN;
 function isBlank(_obj) {
     if(_obj === undefined) { // 只能用 === 运算来测试某个值是否是未定义的
-        console.log("为undefined");
         return true;
     }
 
     if(_obj == null) { // 等同于 _obj === undefined || _obj === null
-        console.log("为null");
         return true;
     }
 
     // String
-    if(_obj == "" || _obj == null || _obj == undefined){ // "",null,undefined
-        console.log("为空");
+    if(_obj === ""){ // "",null,undefined
         return true;
     }
 
     if(!_obj){ // "",null,undefined,N_objN
-        console.log("为空");
         return true;
     }
 
     if(!$.trim(_obj)){ // "",null,undefined
-        console.log("为空");
         return true;
     }
 
     // _objrr_objy
-    if(_obj.length == 0){ // "",[]
-        console.log("为空");
+    if(_obj.length === 0){ // "",[]
         return true;
     }
 
     if(!_obj.length){ // "",[]
-        console.log("为空");
         return true;
     }
 
     // Object {}
     if($.isEmptyObject(_obj)){ // 普通对象使用 for...in 判断，有 key 即为 f_objlse
-        console.log("为空");
         return true;
     }
 
