d2fb5c2c965cb34733ef1df2c5842ebf151ed2a5
ZhengYouzhen <1006747577@qq.com>
2018-12-20 14:37:14 +0800

    文件上传

--- src/main/java/com/geo/geomantic/module/controller/UploadController.java
+++ src/main/java/com/geo/geomantic/module/controller/UploadController.java
@@ -3,6 +3,7 @@ package com.geo.geomantic.module.controller;
 import com.geo.geomantic.common.config.Global;
 import com.geo.geomantic.common.utils.DateUtils;
 import com.geo.geomantic.common.utils.FileUtils;
+import com.geo.geomantic.common.utils.SecretUtils;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,22 +23,20 @@ import java.io.IOException;
 public class UploadController {
 
     @ResponseBody
-    @RequestMapping(value = "fileUpload", method = RequestMethod.POST)
-    public String analyzeXml(@RequestParam("file") MultipartFile file, @RequestParam("id") String id,
-                             HttpServletRequest request, HttpServletResponse response) {
-        StringBuffer retur = new StringBuffer("");
+    @PostMapping("fileUpload")
+    public String analyzeXml(@RequestParam("file") MultipartFile file, HttpServletRequest request, HttpServletResponse response) {
+        StringBuilder retur = new StringBuilder("");
         // boolean result = true;// 导入标志
         if (!file.isEmpty()) {
             // 判断是否为空
 
-            if (true) {
                 // 上传
                 try {
-//                    TODO 拼接文件内容，不推荐使用文件原来名称，这里没写完
-                    String fileName = DateUtils.getDate("yyyyMMdd");
+//                   拼接文件内容，不推荐使用文件原来名称
+                    String fileName = SecretUtils.entryptPassword(file.getOriginalFilename()) + file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
 
                     String leftPath = Global.getUserfilesBaseDir(request);
-                    String rightPath = Global.USERFILES_BASE_URL + File.separator;
+                    String rightPath = Global.USERFILES_BASE_URL + DateUtils.getDate("yyyyMMdd") + File.separator;
                     // 上传文件路径
                     String path = leftPath + rightPath;
                     // 存数据库的路径
@@ -51,10 +50,6 @@ public class UploadController {
                     String realPath = FileUtils.path(path);
                     FileUtils.createDirectory(realPath);
 
-                    // 创建上传文件的目录
-                    String stuRealPath = FileUtils.path(leftPath);
-                    FileUtils.createDirectory(FileUtils.path(stuRealPath));
-
                     File tmp = new File(realPath + fileName);
 
                     FileCopyUtils.copy(file.getInputStream(), new FileOutputStream(tmp));
@@ -67,9 +62,6 @@ public class UploadController {
                 } finally {
 
                 }
-            } else {
-                retur.append("空");
-            }
 
         } else {
             retur.append("空");
