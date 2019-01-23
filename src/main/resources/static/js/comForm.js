4e0104a6067b85fb13d8da2bb3eec95de7ccdaef
Yaolz <478095694@qq.com>
2019-01-23 21:46:59 +0800

    商品图片上传

--- src/main/resources/static/js/comForm.js
+++ src/main/resources/static/js/comForm.js
@@ -12,6 +12,16 @@ layui.use(['form', 'layedit','upload'], function () {
                 return '请选择封面图！';
             }
         },
+        pro_img: function (value) {
+            if(value==null||value==''){
+                return '请选择商品图！';
+            }
+        },
+        pro_imgs: function (value) {
+            if(value==null||value==''){
+                return '请选择商品详情图！';
+            }
+        },
         number: [/^[0-9]*$/, '只能输入数字！'],
         price:[/^[1-9]\d*(\.\d{1,2})?$|^[0]\.\d{1,2}$/g,'请输入有效价格！']
 
@@ -41,6 +51,7 @@ layui.use(['form', 'layedit','upload'], function () {
             if (res > 0) {
                 return layer.msg('上传失败！');
             } else {
+                $('#demoText').empty();
                 return $('#firstImg').val(res.data.src);
             }
             //上传成功
@@ -54,7 +65,37 @@ layui.use(['form', 'layedit','upload'], function () {
             });
         }
     });
+    //多图片上传
+    upload.render({
+        elem: '#checkImgs'
+        ,url: '/uploadFile'
+        ,multiple: true
+        ,before: function(obj){
+            //预读本地文件示例，不支持ie8
+            obj.preview(function(index, file, result){
+                // $('#demo2').append('<img src="'+ result +'" alt="'+ file.name +'" class="layui-upload-img">')
+            });
+        }
+        ,done: function(res){
+            if (res > 0) {
+                return layer.msg('上传失败！');
+            } else {
+                var srcData = res.data.src;
+                $('#demo2').append('<div style="float:left" id="'+srcData.split("/")[5].split(".")[0]+'div">' +
+                    '<a href=\"javascript:;\" style="cursor:pointer" onclick="upImg('+"'"+srcData+"'"+','+"'"+srcData.split("/")[5].split(".")[0]+"'"+')" id ="'+srcData.split("/")[5].split(".")[0]+'"><img src="'+ srcData+'"  class="layui-upload-img"></a><br />' +
+                    '<a href=\"javascript:;\" style="cursor:pointer" onclick="deleteImg('+"'"+srcData+"'"+','+"'"+srcData.split("/")[5].split(".")[0]+"'"+')"><i class="layui-icon">&#xe640;</i></a>' +
+                    '</div>')
 
+                var imgStr = $("#firstImgs").val();
+                if(imgStr!=null&&imgStr!=''){
+                  imgStr =  imgStr+","+srcData;
+                }else {
+                    imgStr = srcData;
+                }
+                return $('#firstImgs').val(imgStr);
+            }
+        }
+    });
 
     // 编辑器上传图片接口
     layedit.set({
@@ -76,4 +117,51 @@ layui.use(['form', 'layedit','upload'], function () {
             , 'image' //插入图片
         ]
     });
+    var firstImgs =$("#firstImgs").val();
+    if(firstImgs!=null&&firstImgs!=''){
+        var data = firstImgs.split(',');
+        for(var i=0,size = data.length; i < size; i++){
+            $('#demo2').append('<div style="float:left;" id="'+data[i].split("/")[5].split(".")[0]+'div">' +
+                '<a href=\"javascript:;\" style="cursor:pointer" onclick="upImg('+"'"+data[i]+"'"+','+"'"+data[i].split("/")[5].split(".")[0]+"'"+')" id ="'+data[i].split("/")[5].split(".")[0]+'"><img src="'+ data[i] +'" class="layui-upload-img"></a><br />' +
+                '<a href=\"javascript:;\" style="cursor:pointer" onclick="deleteImg('+"'"+data[i]+"'"+','+"'"+data[i].split("/")[5].split(".")[0]+"'"+')"><i class="layui-icon">&#xe640;</i></a>' +
+                '</div>')
+        }
+    }
+    window.upImg = function (value,id) {
+        //图片更新
+        upload.render({
+            elem: '#'+id
+            ,url: '/uploadFile'
+            ,before: function(obj){
+                //预读本地文件示例，不支持ie8
+                obj.preview(function(index, file, result){
+                    console.log(result)
+                    document.getElementById(id).getElementsByTagName("img")[0].src=result;
+                    $("#"+id+" img").attr("src",result);//修改选中的图片
+                    // $('#'+id).attr('src',result)
+                });
+            }
+            ,done: function(res){
+                if (res > 0) {
+                    return layer.msg('上传失败！');
+                } else {
+                    var imgStr = $("#firstImgs").val();
+                    if(imgStr!=null&&imgStr!=''){
+                        imgStr = imgStr.replace(value,res.data.src);
+                    }
+                    return $('#firstImgs').val(imgStr);
+                }
+            }
+        });
+    }
+    window.deleteImg = function (value,id) {
+        var div = document.getElementById(id+"div");
+        if (div != null)
+            div.parentNode.removeChild(div);
+        var imgStr = $("#firstImgs").val();
+        if(imgStr!=null&&imgStr!=''){
+            imgStr = imgStr.replace(value+',','');
+    }
+
+    }
 });
