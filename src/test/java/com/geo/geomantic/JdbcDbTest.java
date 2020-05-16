df82d44f7ac7b057aac4d23145c2329d1c097aec
ZhengYouzhen <1006747577@qq.com>
2020-05-16 17:12:38 +0800

    test2

--- src/test/java/com/geo/geomantic/JdbcDbTest.java
+++ src/test/java/com/geo/geomantic/JdbcDbTest.java
@@ -47,6 +47,7 @@ public class JdbcDbTest {
                 stmt.execute();
             }
             System.out.println("test123");
+            System.out.println("test2");
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
